fa47b8547385f33d940e992a7cfcaa3165d82c7f
S Manikanta Varshit <smvarshit@gmail.com>
2024-07-02 12:52:53 +0530

    Added get-jobs functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [jobs, setJobs] = useState([]);
 
   let but = () =>{
       axios.get('http://localhost:3010/thar') 
@@ -13,10 +16,36 @@ function App() {
       })
   }
 
+  let job_list = () =>{
+    axios.get('http://localhost:3010/get-jobs') 
+    .then((response)=>{
+      console.log("GET REQUEST PERFECT");
+      setJobs(response.data);
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
+}
+
   return (
     <div className="App">
       Hi there
       <button onClick={but}>Click me! </button>
+      <br></br>
+      For all list of jobs: 
+      <button onClick={job_list}>Click me! </button>
+      {jobs.length > 0 && (
+      <div>
+        <h2>Jenkins Jobs</h2>
+        <ul>
+          {jobs.map((job, index) => (
+            <li key={index}>{job.name} - {job.color}</li>
+          ))}
+        </ul>
+      </div>
+    )}
+
+
     </div>
   );
 }
